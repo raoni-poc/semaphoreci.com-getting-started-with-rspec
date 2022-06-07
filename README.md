c9b26437f2f0bb436a2cdd6929cbfe93f66d6d53
Raoni Botelho Sporteman <raonibs@gmail.com>
2022-06-07 16:42:03 -0300

    tuto finished and workd fine

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # semaphoreci.com-getting-started-with-rspec
+
+https://semaphoreci.com/community/tutorials/getting-started-with-rspec
+
+```
+bundle init
+rspec --init
+bundle install --path .bundle
+bundle exec rspec
+bundle exec rspec --format documentation
+```
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,29 @@
+require 'string_calculator'
+
+describe StringCalculator do
+  #By convention, class methods are prefixed with a dot (".add"), and instance methods with a dash ("#add").
+  describe ".add" do
+    context "given an empty string" do
+      it "returns zero" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+    context "given '10'" do
+      it "returns  10" do
+        expect(StringCalculator.add("10")).to eql(10)
+      end
+    end
+    context "two numbers" do
+      context "given '2,4'" do
+        it "returns 6" do
+          expect(StringCalculator.add("2,4")).to eql(6)
+        end
+      end
+      context "given '17,100'" do
+        it "returns 117" do
+          expect(StringCalculator.add("17,100")).to eql(117)
+        end
+      end
+    end
+  end
+end
